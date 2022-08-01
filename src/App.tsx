552c2907a44e692a11d7c7619190f216b4efe8ba
Ekaterina <eka.kachan@gmail.com>
2022-08-01 14:21:38 +0400

    feat: add hand-draw animation for svg icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import UserSvg from "./assets/user.svg";
 import "./App.scss";
-import server from "./data/server-ok.json";
 
 type FormData = {
 	FirstName: string;
@@ -102,25 +102,42 @@ function App() {
 					<h1 className="form-container__title-main">Sign up</h1>
 				</header>
 				{success ? (
-						<div className="success-container">
-							<div className="success-container__content-title-subtitle ">
-								<h1 className="title">Thank you</h1>
-								<p className="subtitle">you registered!</p>
-							</div>
-							<div className="form-container__content-submit">
-								<div className="form-container__content-login">
-									<p>
-										Have an account?{" "}
-										<button className="btn-login" onClick={openForm}>
-											Login
-										</button>
-									</p>
-								</div>
+					<div className="success-container">
+						<div className="success-container__content-title-subtitle ">
+							<h1 className="title">Thank you</h1>
+							<p className="subtitle">you registered!</p>
+						</div>
+						<div className="form-container__content-submit">
+							<div className="form-container__content-login">
+								<p>
+									Have an account?{" "}
+									<button className="btn-login" onClick={openForm}>
+										Login
+									</button>
+								</p>
 							</div>
 						</div>
-					) : (
-				<section className="form-container__content">
-					
+					</div>
+				) : (
+					<section className="form-container__content">
+						<div className="svg-bkg">
+						<svg
+							width="307"
+							height="355"
+							viewBox="0 0 307 355"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+							fill="none" stroke="#F9FAFF" stroke-width="6"
+								className="pathOne"
+								fill-rule="evenodd"
+								d="M205.5 411C92.236 411 0 318.764 0 205.5C0 92.236 92.236 0 205.5 0C318.764 0 411 92.236 411 205.5C411 271.362 379.812 330.113 331.438 367.754C333.781 365.394 335.295 362.201 335.521 358.578C336 354.751 336 351.881 336 348.533V343.749C335.879 337.683 335.35 331.725 334.44 325.901C363.927 294.343 382 251.99 382 205.5C382 108.186 302.814 29 205.5 29C108.186 29 29 108.186 29 205.5C29 252.377 47.3746 295.047 77.2988 326.686C76.282 333.801 75.8485 341.1 76.0469 348.533C76.0469 351.403 76.0469 354.751 76.5256 358.578C77.0044 366.71 83.7067 372.451 91.8452 371.972C99.9836 371.494 105.728 364.797 105.25 356.665C104.892 354.163 104.802 352.196 104.779 350.363C133.361 370.298 168.091 382 205.5 382C243.357 382 278.471 370.016 307.247 349.643C307.207 350.468 307.131 351.336 307.053 352.232C306.928 353.664 306.797 355.164 306.797 356.665C306.318 364.318 312.063 371.494 320.202 371.972H321.159C323.791 371.972 326.253 371.294 328.371 370.094C294.07 395.773 251.521 411 205.5 411ZM307.247 349.643C317.103 342.665 326.215 334.703 334.44 325.901C324.506 262.367 269.17 214.712 203.39 216.027C138.589 217.313 86.0983 265.111 77.2988 326.686C85.6155 335.48 94.8243 343.421 104.779 350.363C104.771 349.743 104.771 349.138 104.771 348.533V344.227C105.728 288.259 152.166 243.771 208.178 245.207C264.19 246.163 308.712 292.564 307.276 348.533C307.276 348.892 307.265 349.263 307.247 349.643ZM328.371 370.094C329.401 369.324 330.423 368.543 331.438 367.754C330.531 368.667 329.5 369.455 328.371 370.094ZM147 142.5C147 174.339 172.661 200 204.5 200C236.339 200 262 174.339 262 142.5C262 110.661 236.339 85 204.5 85C172.661 85 147 110.661 147 142.5ZM205.5 114C189.057 114 176 127.057 176 143.5C176 159.943 189.057 173 205.5 173C221.943 173 235 159.943 235 143.5C235 127.057 221.943 114 205.5 114Z"
+						
+							/>
+						</svg>
+						</div>
+						
 						<form onSubmit={handleSubmit(handleSubmitForm)}>
 							<div className="form-container__content-title-subtitle">
 								<h1 className="title">New user?</h1>
@@ -365,8 +382,7 @@ function App() {
 								</div>
 							</div>
 						</form>
-					
-				</section>
+					</section>
 				)}
 			</div>
 		</main>
